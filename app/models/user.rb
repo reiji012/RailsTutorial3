63fbcff4a639aee2850e0b7af7f6a8d8fe167e8f
Reiji <reiji0120124@gmail.com>
2018-09-29 16:13:22 +0900

    Make a basic User model (including secure passwords)

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
     validates :name, presence: true, length: { maximum:50 }
     VALIDE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum:255 }, format: {with: VALIDE_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
+
+    has_secure_password
+
+    validates :password, presence: true, length: { minimum: 6 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180929054029) do
+ActiveRecord::Schema.define(version: 20180929062056) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
+    t.string "password_digest"
   end
 
 end
