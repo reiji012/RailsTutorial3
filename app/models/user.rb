cd630182a94c3a5229dae66917ccd91cfb4ba40b
Reiji <reiji0120124@gmail.com>
2018-09-29 14:05:52 +0900

    add email validate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum:50 }
+    VALIDE_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum:255 }, format: {with: VALIDE_EMAIL_REGEX}, uniqueness: true
 end
