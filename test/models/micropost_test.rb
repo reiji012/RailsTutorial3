98457d684a88c12927a6dfcf3a3b5aad6263f9ab
ReijiMatsumura <p.matsumura@ailes-dor.co.jp>
2018-10-16 18:56:04 +0900

    13.1.2

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -14,5 +14,15 @@ class MicropostTest < ActiveSupport::TestCase
 		@micropost.user_id = nil
 		assert_not @micropost.valid?
 	end
+
+	test "content should be present" do
+		@micropost.content = " "
+		assert_not @micropost.valid?
+	end
+
+	test "content should be most 140 caracters" do
+		@micropost.content = "a" * 141
+		assert_not @micropost.valid?
+	end
 	
 end
